f270fdb316cb7e373ee4f8073ebc7deca8ef298d
shikato <nnilan2@gmail.com>
2017-09-18 11:30:47 +0900

    :sparkles: 表示する作品数を増やす機能を削除 #14

--- pixiv_sk.js
+++ pixiv_sk.js
@@ -2,178 +2,80 @@
 // @name           pixiv_sk
 // @namespace      http://alexam.hateblo.jp/
 // @author         shikato
-// @description    pixivの検索結果をソートしたりフィルタリングしたり1ページに表示する数を増やしたりできます。
+// @description    pixivの検索結果をフィルタリングしソートします。
 // @version        1.1.1
 // @include        https://www.pixiv.net/search.php*
 // @include        https://www.pixiv.net/tags.php*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // ==/UserScript==
 
-(function (doc, func) {
 
-  var head = doc.getElementsByTagName('head')[0]; 
+/** 設定値 ここから **/
 
-  var jquery = doc.createElement('script'); 
-  jquery.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
-  jquery.addEventListener('load', function() {
-    var myScript = doc.createElement('script');
-    myScript.textContent = 'jQuery.noConflict();(' + func.toString() + ')(jQuery);';
-    head.appendChild(myScript);
-  }, false); 
+// 作品のブックマーク数が以下の値未満の場合は表示しない
+var FAV_FILTER = 3;
+// リンクを別のタブで開くかどうか true / false
+var IS_LINK_BLANK = true; 
 
-  head.appendChild(jquery);
-})(document, function ($) {
+/** 設定値 ここまで **/ 
 
-  /** 設定値 ここから **/
-  
-  // 1ページに何ページ分の作品を表示するか
-  // ex) 1なら通常通り
-  //     2にすると2ページ分表示
-  var GETTING_PAGE_COUNT = 3;
-  // 作品のブックマーク数が以下の値未満の場合は表示しない
-  var FAV_FILTER = 3;
-  // リンクを別のタブで開くかどうか true / false
-  var IS_LINK_BLANK = true; 
 
-  /** 設定値 ここまで **/ 
+if (FAV_FILTER < 0) {
+  return;
+}
 
-  var LOADING_IMG = 'https://raw.githubusercontent.com/shikato/pixiv_sk/master/loading.gif';
+var getFav = function (target) { 
+  var favText = target.children('figure').children('figcaption').children('ul').children('li:lt(3)').children('ul').children('li').children('a').text(); 
+  if (favText === '') {
+    return 0;
+  } else { 
+    return parseInt(favText);
+  }
+};
 
-  if (GETTING_PAGE_COUNT < 1 || FAV_FILTER < 0) return;
+// worksをフィルタリングしてソートしてHTML(文字列)を返す
+var filterAndSort = function () {
+  var works = []; 
 
-  var mCurrentGettingPageCount = null;
-  var mCurrentUrl = null;
-  var mCurrentPage = null;
-  var mWorks = [];
-
-  // mCurrentPageの作品を取得する
-  var getWorks = function (onloadCallback) { 
-    var url = mCurrentUrl;
-
-    if (mCurrentPage === 1) {
-      url += ('&p='+mCurrentPage); 
-    } else {
-      url = mCurrentUrl.replace(/p=\d+/, 'p='+mCurrentPage); 
-    } 
-    mCurrentUrl = url; 
-
-    var req = new XMLHttpRequest();
-    req.open('GET', mCurrentUrl, true);
-    req.onload = function (event) {
-      onloadCallback(req); 
-      req = null;
-    };
-    req.onerror = function (event) {
-      alert('作品の取得に失敗しました。');
-      req = null;
-    };
-
-    req.send(null); 
-  };
-  
-  // mWorksをフィルタリングしてソートしてHTML(文字列)を返す
-  var filterAndSort = function () {
-    // FAV_FILTER未満の作品をremove 
-    mWorks.forEach(function (work, i) { 
-      var fav = work.children('ul').children('li:first').children('a').text();
-      if (fav < FAV_FILTER) {
-        mWorks.splice(i, 1);
-      } else {
-        // blank onの場合 target属性追加
-        if (!IS_LINK_BLANK) return;
-        work.children('a').attr('target', 'blank');
+  // FAV_FILTER未満の作品をremove
+  $('._2xrGgcY').children('._7IVJuWZ').each(function() { 
+    var fav = getFav($(this)); 
+    if (fav >= FAV_FILTER) { 
+      // blank onの場合 target属性追加
+      if (!IS_LINK_BLANK) {
+        return;
       }
-    });
-
-    // ソート 
-    mWorks.sort(function (a, b) {
-      var favA = a.children('ul').children('li:first').children('a').text();
-      var favB = b.children('ul').children('li:first').children('a').text();
-      if (favA === '') {
-        favA = 0; 
-      } else {
-        favA = parseInt(favA); 
-      }
-      if (favB === '') {
-        favB = 0; 
-      } else {
-        favB = parseInt(favB); 
-      }
-      if (favA > favB) {
-        return -1;
-      }
-      if (favA < favB) {
-        return 1;
-      }
-      return 0;
-    }); 
-
-    var results = ''; 
-    for (var i = 0; i < mWorks.length; i++) { 
-      results += $('<div>').append(mWorks[i]).html();                    
+      $(this).children('figure').children('div').children('a').attr('target', 'blank').attr('rel', '');
+      works.push($(this));
     }
+  });
 
-    return results;
-  }; 
+  // ソート 
+  works.sort(function (a, b) {
+    var favA = getFav(a); 
+    var favB = getFav(b); 
+    if (favA > favB) {
+      return -1;
+    }
+    if (favA < favB) {
+      return 1;
+    }
+    return 0;
+  }); 
 
-
-  mCurrentGettingPageCount = 0;
-  mCurrentUrl = location.href;
-  mCurrentPage = mCurrentUrl.match(/p=(\d+)/); 
-
-  if (mCurrentPage !== null) {
-    mCurrentPage = parseInt(mCurrentPage[1]);
-  } else {
-    mCurrentPage = 1;
+  var results = ''; 
+  for (var i = 0; i < works.length; i++) { 
+    results += $('<div>').append(works[i]).html();                    
   }
 
-  if (GETTING_PAGE_COUNT > 1) {
-    // load時のスピナー表示
-    $('.column-search-result').children('ul').hide(); 
-    $('.column-search-result').prepend(
-      '<div id="loading" style="width:50px;margin-left:auto;margin-right:auto;">'
-       + '<img src="' + LOADING_IMG + '" /></div>'
-    );
+  return results;
+}; 
 
-    // pixiv_sk用のページネーションリンク表示
-    if (mCurrentPage === 1) {
-      $('.pager-container').empty().append(
-        '<a href="' + mCurrentUrl + '" style="margin-right:15px;">&lt;&lt;</a>'
-         + '<a href="' + mCurrentUrl + '&p=' + (mCurrentPage+GETTING_PAGE_COUNT) +'">&gt;</a>'
-      ); 
-    } else {
-      $('.pager-container').empty().append(
-        '<a href="'+mCurrentUrl.replace(/&p=\d+/, '') + '" style="margin-right:15px;">&lt;&lt;</a>'
-         + '<a href="'+mCurrentUrl.replace(/p=\d+/, 'p=' + (mCurrentPage - GETTING_PAGE_COUNT)) + '" style="margin-right:10px;">&lt;</a>'
-         + '<a href="'+mCurrentUrl.replace(/p=\d+/, 'p=' + (mCurrentPage + GETTING_PAGE_COUNT)) + '" style="margin-right:10px;">&gt;</a>'
-      ); 
-    }
-
-    var onloadCallback = function (req) { 
-      $(req.responseText).find('.column-search-result').children('._image-items').children('.image-item').each(function() {
-        var thumb = $(this).children('.work').children('._layout-thumbnail').children('._thumbnail');
-        thumb.attr('src', thumb.attr('data-src')); 
-        mWorks.push($(this));
-      }); 
-
-      mCurrentPage++; 
-      mCurrentGettingPageCount++; 
-      // GETTING_PAGE_COUNTで指定した分だけ作品を取得したらソートして表示
-      if (mCurrentGettingPageCount === GETTING_PAGE_COUNT) {
-        $('#loading').remove();
-        var sortedImages = filterAndSort();
-        $('.column-search-result').children('ul').empty().append(sortedImages).show(); 
-      } else { 
-        getWorks(onloadCallback);
-      } 
-    };
-    
-    getWorks(onloadCallback);
-  } else {
-    // filterAndSortだけ実行して表示
-    var sortedImages = filterAndSort(); 
-    $('.column-search-result').children('ul').empty().append(sortedImages);
+var pisivSkInterval = setInterval(function () {
+  if ($('._2xrGgcY').length) { 
+    var sortedWorks = filterAndSort(); 
+    $('._2xrGgcY').empty().append(sortedWorks).show(); 
+    clearInterval(pisivSkInterval); 
+    return;
   }
-
-  // 被さって表示されてしまう要素をremove
-  $('.popular-introduction').remove();
-});
+}, 5); 
